9851bafe6e3a1be58c45a9c0f9e377de5c520d4f
Hzsa-anony <115693753+Hzsa-anony@users.noreply.github.com>
2024-10-15 22:46:23 +0800

    Delete __ENV.js

--- __ENV.js
+++
@@ -1,9 +0,0 @@
-window.__ENV = {
-    "NEXT_PUBLIC_USE_AUTH_FIREBASE": "false",
-    "NEXT_PUBLIC_FIREBASE_MESSAGGING_VAPID_KEY": "BL9OAtOPd-91qphOZgZftbsJxZiS-y8_qJr995W0rgfbc_A6tlfC5C-1Xq40K7UFxKt8Ali3hTCr8br64X-KFZI",
-    "NEXT_PUBLIC_RECAPTHA_SITE_KEY": "6LfhlUwlAAAAAOa_Fgslju3ujuyqpfztRR3Ofuxs",
-    "NEXT_PUBLIC_SOCKET_PREFIX": "takapedia",
-    "NEXT_PUBLIC_BE_URL": "https://bj-api.ardpedia.com/v3",
-    "NEXT_PUBLIC_FE_URL": "https://www.ardpedia.com",
-    "NEXT_PUBLIC_SOCKET_URL": "https://echo.salmonaru.com"
-};
